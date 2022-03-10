67ca6af16589c2423b4442848d9b61a08ab1e129
Sara-Barbi <barbisara02@gmail.com>
2022-03-10 18:01:31 +0100

    create button

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,1 +1,24 @@
-creare post
+@extends('layouts.base')
+
+@section('title','comics list')
+
+@section('content')
+<form action="{{route('comics.store')}}" method='post'> 
+    @csrf
+    <label for="title">Title</label>
+    <input type="text" name="title" id="title">
+
+    <label for="series">Series</label>
+    <input type="text" name="series" id="series">
+
+    <label for="thumb">Image</label>
+    <input type="text" name="thumb" id="thumb">
+
+    <label for="description">Description</label>
+    <input type="text" name="description" id="description">
+
+    <label for="price">Price</label>
+    <input type="text" name="price" id="price">
+    <button type="submit"><a href="">Aggiungi</a></button>
+</form>
+@endsection
