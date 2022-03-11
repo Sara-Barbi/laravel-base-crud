889e56b452b50f381e97ec1ce25df91b05b6971f
Sara-Barbi <barbisara02@gmail.com>
2022-03-11 16:54:35 +0100

    css create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -3,29 +3,29 @@
 @section('title','comics list')
 
 @section('content')
-<form action="{{ route("comics.store") }}" method='POST'> 
+<form action="{{ route("comics.store") }}" class="text-center d-flex flex-column align-items-center" method='POST'> 
     @csrf
-    <label for="title">Title</label>
-    <input type="text" name="title" id="title">
+    <label for="title" class="form-label m-3">Title</label>
+    <input type="text" name="title" id="title" class="form-control col-6 text-center " placeholder="Username">
 
-    <label for="series">Series</label>
-    <input type="text" name="series" id="series">
+    <label for="series" class="form-label m-3">Series</label>
+    <input type="text" name="series" id="series" class="form-control col-6 text-center " placeholder="Username">
 
-    <label for="thumb">Image</label>
-    <input type="text" name="thumb" id="thumb">
+    <label for="thumb" class="form-label m-3">Image</label>
+    <input type="text" name="thumb" id="thumb" class="form-control col-6 text-center " placeholder="Username">
 
-    <label for="description">Description</label>
-    <input type="text" name="description" id="description">
+    <label for="description"class="form-label m-3">Description</label>
+    <input type="text" name="description" id="description"class="form-control col-6 text-center " placeholder="Username">
 
-    <label for="price">Price</label>
-    <input type="text" name="price" id="price">
+    <label for="price" class="form-label m-3">Price</label>
+    <input type="text" name="price" id="price" class="form-control col-6 text-center " placeholder="Username">
     
-    <label for="sale_date">Sale Date(yyyy-mm-gg)</label>
-    <input type="text" name="sale_date" id="sale_date">
+    <label for="sale_date" class="form-label m-3">Sale Date(yyyy-mm-gg)</label>
+    <input type="text" name="sale_date" id="sale_date" class="form-control col-6 text-center " placeholder="Username">
 
-    <label for="type">Tipology</label>
-    <input type="text" name="type" id="type">
+    <label for="type" class="form-label m-3">Tipology</label>
+    <input type="text" name="type" id="type" class="form-control col-6 text-center " placeholder="Username">
 
-    <button type="submit">Aggiungi</button>
+    <button type="submit" class="btn btn-success m-5">Aggiungi</button>
 </form>
 @endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,7 +3,7 @@
 @section('title','comics list')
 
 @section('content')
-<a href="{{route("comics.create")}}"><button type="button" class="btn btn-success">aggiungi</button></a>  <!--questo bottone se cliccato reindirizza alla route comics/create, possiamo vedere la rotta con php artisan route:list-->
+<a href="{{route("comics.create")}}"><button type="button" class="btn btn-success">Aggiungi</button></a>  <!--questo bottone se cliccato reindirizza alla route comics/create, possiamo vedere la rotta con php artisan route:list-->
     @foreach($comics as $comic)
         <h1>{{$comic->title}}</h1>
         <p><strong>Series:</strong> {{$comic->series}}</p>
