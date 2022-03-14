8f0b8fd0b12deff719aef1ae93da0d0e9b795151
Sara-Barbi <barbisara02@gmail.com>
2022-03-14 17:03:50 +0100

    controllo validate create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -13,33 +13,41 @@
     @enderror
 
     <label for="series" class="form-label m-3">Series</label>
-    <input type="text" name="series" id="series" class="form-control col-6 text-center " placeholder="Insert Series">
+    <input type="text" name="series" id="series" class="form-control col-6 text-center " placeholder="Insert Series" value="{{old("series")}}">
+    @error('series')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
     <label for="thumb" class="form-label m-3">Image</label>
-    <input type="text" name="thumb" id="thumb" class="form-control col-6 text-center " placeholder="Insert Image url">
+    <input type="text" name="thumb" id="thumb" class="form-control col-6 text-center " placeholder="Insert Image url" value="{{old("thumb")}}">
+    @error('thumb')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
     <label for="description"class="form-label m-3">Description</label>
-    <input type="text" name="description" id="description"class="form-control col-6 text-center " placeholder="Insert Description">
+    <input type="text" name="description" id="description"class="form-control col-6 text-center " placeholder="Insert Description" value="{{old("description")}}">
+    @error('description')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
     <label for="price" class="form-label m-3">Price</label>
-    <input type="text" name="price" id="price" class="form-control col-6 text-center " placeholder="Insert Price">
-    
+    <input type="text" name="price" id="price" class="form-control col-6 text-center " placeholder="Insert Price" value="{{old("title")}}">
+    @error('title')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
+
     <label for="sale_date" class="form-label m-3">Sale Date</label>
-    <input type="text" name="sale_date" id="sale_date" class="form-control col-6 text-center " placeholder="Insert yyyy-mm-gg">
+    <input type="text" name="sale_date" id="sale_date" class="form-control col-6 text-center " placeholder="Insert yyyy-mm-gg" value="{{old("price")}}">
+    @error('price')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
     <label for="type" class="form-label m-3">Tipology</label>
-    <input type="text" name="type" id="type" class="form-control col-6 text-center " placeholder="Insert Tipology">
+    <input type="text" name="type" id="type" class="form-control col-6 text-center " placeholder="Insert Tipology" value="{{old("type")}}">
+    @error('type')
+        <div class="alert alert-danger">{{ $message }}</div>
+    @enderror
 
     <button type="submit" class="btn btn-success m-5">Aggiungi</button>
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
 </form>
 @endsection
